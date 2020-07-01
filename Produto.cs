04b39566ea693fc6a8d91e7c3758b2641a1ae1ea
Gabriel Ferreira <you@example.com>
2020-06-30 23:31:06 -0300

    Ver lista

--- Produto.cs
+++ Produto.cs
@@ -1,35 +1,73 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Excel_27_28_29_30
 {
     public class Produto
     {
-        public int Codigo {get; set;}
-        public string Nome {get; set;}
-        public float Preco {get; set;}
-        
-        private const string PATH = "DataBase/Produto.csv";
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        private const string PATH = "Database/produto.csv";
 
         public Produto()
         {
-            //Cria o arquivo caso não exista
-            if(!Directory.Exists(PATH))
+            string pasta = PATH.Split('/')[0];
+            if (!Directory.Exists(pasta))
             {
-                Directory.CreateDirectory(PATH);
+                Directory.CreateDirectory(pasta);
             }
+
+
             //Cria o arquivo caso não exista
-            if(!File.Exists(PATH))
+            if (!File.Exists(PATH))
             {
                 File.Create(PATH).Close();
             }
         }
-        public void Inserir(Produto p){
-            var linha = new string [] { p.PrepararLinhaCSV(p)};
+        public void Inserir(Produto p)
+        {
+            var linha = new string[] { p.PrepararLinhaCSV(p) };
             File.AppendAllLines(PATH, linha);
         }
-        private string PrepararLinhaCSV(Produto produto){
+        private string PrepararLinhaCSV(Produto produto)
+        {
             return $"codigo={produto.Codigo};nome={produto.Nome};preço={produto.Preco}";
+        }
+
+        // Criamos lista 
+        List<Produto> prod = new List<Produto>();
+
+        public List<Produto> Ler()
+        {
+            string[] linhas = File.ReadAllLines(PATH);
+
+            // Dados novos produtos
+            foreach (string linha in linhas)
+            {
+                string[] dados = linha.Split(";");
+
+                Produto p = new Produto();
+                p.Codigo = Int32.Parse(dados[0].Split("=")[1]);
+                p.Nome = dados[1].Split("=")[1];
+                p.Preco = float.Parse(dados[2].Split("=")[1]);
+
+                prod.Add(p);
+            }
+            return prod;
+        }
+        public string Separar(string dados)
+        {
+            // codigo=1
+            // [0] = codigo
+            // [1] = 1
+            return dados.Split("=")[1];
+        }
+        private string PrepararLinha(Produto p)
+        {
+            return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Excel_27_28_29_30
 {
@@ -7,11 +8,18 @@ namespace Excel_27_28_29_30
         static void Main(string[] args)
         {
             Produto p1 = new Produto();
-            p1.Codigo = 1;
-            p1.Nome = "PS5";
-            p1.Preco = 5000.00f;
+            p1.Codigo = 2;
+            p1.Nome = "PS4";
+            p1.Preco = 3000f;
 
             p1.Inserir(p1);
+
+            List<Produto> lista = p1.Ler();
+
+            foreach (Produto item in lista)
+            {
+                System.Console.WriteLine($" {p1.Preco} - {p1.Nome}");
+            }
         }
     }
 }
